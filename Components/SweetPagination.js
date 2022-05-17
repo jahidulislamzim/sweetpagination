f9757faecc01158cc648ebc50b672af943e9e370
jahidulislamzim <jahidulislamzim845@gmail.com>
2022-05-17 17:08:06 +0600

    added readme

--- Components/SweetPagination.js
+++ Components/SweetPagination.js
@@ -1,20 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, {Fragment, useEffect, useState } from "react";
 import "./Components/SweetPagination.css";
 
-const SweetPagination = ({ dataPerPage, getData, currentPageData, navigation }) => {
+const SweetPagination= ({ dataPerPage, getData, currentPageData, navigation, getStyle }) => {
   const pageNumbers = [];
   const showPagination = [];
   const [currentPage, setCurrentPage] = useState(1);
   const [dataPerPageState, setDataPerPageState] = useState();
   const [navigationState, setNavigationState] = useState(false);
-  const [showPaginationState, setShowPaginationState] = useState(showPagination)
-
-
+  const [showPaginationState, setShowPaginationState] = useState(showPagination);
+  const [style, setStyle]=useState('');
+  const [styleCustom, setStyleCustom] = useState('');
 
   for (let i = 1; i <= Math.ceil(getData.length / dataPerPageState); i++) {
     pageNumbers.push(i);
   }
-
 
 
   useEffect(() => {
@@ -25,8 +24,6 @@ const SweetPagination = ({ dataPerPage, getData, currentPageData, navigation }) 
 
 
   // update item on button chick
-
-
   useEffect(() => {
     // update right to left
     if (currentPage === showPaginationState.at(-1)) {
@@ -55,10 +52,10 @@ const SweetPagination = ({ dataPerPage, getData, currentPageData, navigation }) 
 
 
     //  update left to right 
-    if (pageNumbers.at(1)  <= showPaginationState.at(0)) {
-     
-      if(pageNumbers.at(1) === currentPage){
-        showPaginationState.unshift( currentPage - 1);
+    if (pageNumbers.at(1) <= showPaginationState.at(0)) {
+
+      if (pageNumbers.at(1) === currentPage) {
+        showPaginationState.unshift(currentPage - 1);
         showPaginationState.pop();
       }
       else if (showPaginationState.at(0) === currentPage) {
@@ -67,25 +64,13 @@ const SweetPagination = ({ dataPerPage, getData, currentPageData, navigation }) 
         showPaginationState.pop();
       }
       else if (currentPage === showPaginationState.at(1)) {
-          showPaginationState.unshift(currentPage - 2);
-          showPaginationState.pop();
-        }
-    
+        showPaginationState.unshift(currentPage - 2);
+        showPaginationState.pop();
+      }
+
     }
-  
-  }, [currentPage, ])
 
-
-
-
-
-  console.log(showPaginationState)
-
-
-
-
-
-
+  }, [currentPage,])
 
   //  initialize page data
   useEffect(() => {
@@ -116,10 +101,9 @@ const SweetPagination = ({ dataPerPage, getData, currentPageData, navigation }) 
 
   }
 
-  const handleDirectFirstPage = () =>{
+  const handleDirectFirstPage = () => {
     setShowPaginationState([1, 2, 3, 4, 5])
   }
-
 
   const handleDirectLastPage = () => {
     setShowPaginationState([pageNumbers.at(-5), pageNumbers.at(-4), pageNumbers.at(-3), pageNumbers.at(-2), pageNumbers.at(-1)])
@@ -127,149 +111,159 @@ const SweetPagination = ({ dataPerPage, getData, currentPageData, navigation }) 
 
 
 
+  // button style section 
+
+  useEffect(()=>{
+    if(!getStyle){
+      setStyle('sweetPagination')
+      setStyleCustom('')
+    }
+    else if(getStyle && getStyle !== 'style-custom'){
+      setStyle(getStyle)
+      setStyleCustom('')
+    }
+    else if(getStyle === 'style-custom'){
+      setStyleCustom('style-custom')
+    }
+  
+  },[getStyle])
 
 
 
-  if (pageNumbers.length > 10) {
-    return (
-      <nav>
-        <ul className="pagination">
 
-          {/* preview navigation  */}
-          {navigationState &&
-            (currentPage === 1 ? (
-              <li className="pageItem">
-                <button className="pageButton">P-</button>
-              </li>
-            ) : (
-              <li className="pageItem">
-                <button
-                  onClick={() => handleCurrentPage(currentPage - 1)}
-                  className="pageButton"
-                >
-                  P+
-                </button>
-              </li>
-            ))}
+// Main button function 
 
+if (pageNumbers.length > 10) {
+  return (
+    <nav className={`sweetPagination ${style}`}>
+      <ul className="paginationUL">
 
-
-              {/* first page  */}
-              {
-                showPaginationState.at(0) > pageNumbers.at(0)&&
-                
-               <Fragment>
-                  <li className="pageItem">
-                <button
-                  onClick={() => {handleCurrentPage(pageNumbers.at(0)); handleDirectFirstPage()}}
-                  className={
-                    currentPage === pageNumbers.at(0)
-                      ? "pageButton activeButton"
-                      : "pageButton"
-                  }
-                >
-                  {pageNumbers.at(0)}
-                </button>
-              </li>
-
-              <li className="pageItem">
-                <button className="pageButton">
-                  .....
-                </button>
-              </li>
-               </Fragment>
-}
-              {showPaginationState.map((number) => (
-                <li key={number} className="pageItem">
-                  <button
-                    onClick={() => handleCurrentPage(number)}
-                    className={
-                      currentPage === number
-                        ? "pageButton activeButton"
-                        : "pageButton"
-                    }
-                  >
-                    {number}
-                  </button>
-                </li>
-              ))}
-
-
-              {/* Lase Page  */}
-              { 
-              showPaginationState.at(-1) < pageNumbers.at(-1) &&
-               <Fragment>
-                  <li className="pageItem">
-                <button className="pageButton">
-                  .....
-                </button>
-              </li>
-                  <li className="pageItem">
-                <button
-                  onClick={() =>{ handleCurrentPage(pageNumbers.at(-1)); handleDirectLastPage()}}
-                  className={
-                    currentPage === pageNumbers.at(-1)
-                      ? "pageButton activeButton"
-                      : "pageButton"
-                  }
-                >
-                  {pageNumbers.at(-1)}
-                </button>
-              </li>
-               </Fragment>
-              }
-
-
-        
-
-          {/* {pageNumbers.map((number) => (
-            <li key={number} className="pageItem">
-              <button
-                onClick={() => handleCurrentPage(number)}
-                className={
-                  currentPage === number
-                    ? "pageButton activeButton"
-                    : "pageButton"
-                }
-              >
-                {number}
+        {/* preview navigation  */}
+        {navigationState &&
+          (currentPage === 1 ? (
+            <li className="pageItem">
+              <button className="pageButton commonButtonStyle leftNavigation">
+              <svg className="navigationIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"> <path d="M192 448c-8.188 0-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L77.25 256l137.4 137.4c12.5 12.5 12.5 32.75 0 45.25C208.4 444.9 200.2 448 192 448z" /></svg>
               </button>
             </li>
-          ))} */}
+          ) : (
+            <li className="pageItem">
+              <button
+                onClick={() => handleCurrentPage(currentPage - 1)}
+                className="pageButton commonButtonStyle leftNavigation"
+              >
+                <svg className="navigationIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"> <path d="M192 448c-8.188 0-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L77.25 256l137.4 137.4c12.5 12.5 12.5 32.75 0 45.25C208.4 444.9 200.2 448 192 448z" /></svg>
+              </button>
+            </li>
+          ))}
 
 
-          {/* next navigation  */}
-          {navigationState &&
-            (currentPage === pageNumbers.at(-1) ? (
-              <li className="pageItem">
-                <button className="pageButton">N-</button>
-              </li>
-            ) : (
-              <li className="pageItem">
-                <button
-                  onClick={() => handleCurrentPage(currentPage + 1)}
-                  className="pageButton"
-                >
-                  N+
-                </button>
-              </li>
-            ))}
-        </ul>
-      </nav>
-    );
-  }
+
+        {/* first page  */}
+        {
+          showPaginationState.at(0) > pageNumbers.at(0) &&
+
+          <Fragment>
+            <li className="pageItem">
+              <button
+                onClick={() => { handleCurrentPage(pageNumbers.at(0)); handleDirectFirstPage() }}
+                className={
+                  currentPage === pageNumbers.at(0)
+                    ? "pageButton activeButton"
+                    : "pageButton commonButtonStyle"
+                }
+              >
+                {pageNumbers.at(0)}
+              </button>
+            </li>
+
+            <li className="pageItem">
+              <button className="pageButton commonButtonStyle">
+              <svg className="dottedIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+                  <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
+                </svg> 
+              </button>
+            </li>
+          </Fragment>
+        }
+        {showPaginationState.map((number) => (
+          <li key={number} className="pageItem">
+            <button
+              onClick={() => handleCurrentPage(number)}
+              className={
+                currentPage === number
+                  ? `pageButton activeButton commonButtonStyle`
+                  : `pageButton commonButtonStyle ${styleCustom}`
+              }
+            >
+              {number}
+            </button>
+          </li>
+        ))}
+
+
+        {/* Lase Page  */}
+        {
+          showPaginationState.at(-1) < pageNumbers.at(-1) &&
+          <>
+            <li className="pageItem">
+              <button className="pageButton commonButtonStyle">
+                <svg className="dottedIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+                  <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
+                </svg>
+              </button>
+            </li>
+            <li className="pageItem">
+              <button
+                onClick={() => { handleCurrentPage(pageNumbers.at(-1)); handleDirectLastPage() }}
+                className={
+                  currentPage === pageNumbers.at(-1)
+                    ? "pageButton activeButton"
+                    : `pageButton commonButtonStyle ${styleCustom}`
+                }
+              >
+                {pageNumbers.at(-1)}
+              </button>
+            </li>
+          </>
+        }
+
+
+
+        {/* next navigation  */}
+        {navigationState &&
+          (currentPage === pageNumbers.at(-1) ? (
+            <li className="pageItem">
+              <button className="pageButton commonButtonStyle rightNavigation">
+              <svg className="navigationIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z" /></svg>
+              </button>
+            </li>
+          ) : (
+            <li className="pageItem">
+              <button
+                onClick={() => handleCurrentPage(currentPage + 1)}
+                className="pageButton commonButtonStyle rightNavigation"
+              >
+                <svg className="navigationIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z" /></svg>
+              </button>
+            </li>
+          ))}
+      </ul>
+    </nav>
+  );
+};
 
   return (
-    <nav>
-      <ul className="pagination">
+    <nav className={`sweetPagination ${style}`}>
+      <ul className="paginationUL">
         {pageNumbers.map((number) => (
           <li key={number} className="pageItem">
             <button
               onClick={() => handleCurrentPage(number)}
               className={
                 currentPage === number
-                  ? "pageButton activeButton"
-                  : "pageButton"
+                  ? "pageButton activeButton commonButtonStyle"
+                  : `pageButton commonButtonStyle ${styleCustom}`
               }
             >
               {number}
---
+++ README.md
@@ -0,0 +1,46 @@
+# Sweet Pagination
+
+**A ReactJS component to render a pagination.**
+
+By installing this component and writing only a little bit of code you can obtain this:
+
+
+<img src="https://cloud.githubusercontent.com/assets/2084833/24840237/7accb75a-1d1e-11e7-9abb-818431398b91.png" alt="Pagination demo 2" />
+
+
+## Installation
+
+Install [`Sweet Pagination`](https://www.npmjs.com/package/sweetpagination) with npm:
+
+```
+npm i sweetpagination --save
+```
+
+## Usage
+
+```javascript
+import React, { useState } from "react";
+import SweetPagination from "sweetpagination";
+
+function Items() {
+  const [currentPageData, setCurrentPageData] = useState(new Array(2).fill());
+  // Example items, to simulate fetching from another resources.
+  const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+  return (
+    <div>
+      {currentPageData.map((item) => (
+        <div>
+          <h3>Item #{item}</h3>
+        </div>
+      ))}
+
+      <SweetPagination
+        currentPageData={setCurrentPageData}
+        dataPerPage={10}
+        getData={items}
+        navigation={true}
+      />
+    </div>
+  );
+}
+```
